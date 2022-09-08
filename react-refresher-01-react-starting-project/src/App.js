896cf57974d9a3e26be13b6d8b6d4a0ebb6b6778
Rayho564 <rayho564@gmail.com>
2022-09-08 00:00:13 -0700

    States and handlers

--- react-refresher-01-react-starting-project/src/App.js
+++ react-refresher-01-react-starting-project/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// use useState to manage the state of props to update
+import React, { useState } from 'react';
 
 // This also works the same above
 // class App extends React.Component {
@@ -24,17 +25,36 @@ import NewGoal from './components/NewGoal/NewGoal';
 
 
 const App = () => {
-  const courseGoals = [
+  // first param, the initial state of variables
+  // second param, a function to update the state and rerender (usually begins with set)
+  const [courseGoals, setCourseGoals] = useState([
     {id: 'cg1', text: 'Finish the Course'},
     {id: 'cg2', text: 'Learn all about the Course Main Topic'},
     {id: 'cg3', text: 'Help other students in the Course Q&A'},
-  ];
+  ]);
 
-  return <div className="course-goals">
+  const addNewGoalHandler = newGoal => {
+    // be careful with state updates as it's handled by react at certain timings
+      // if there's a bunch of stuff going on then there could be a lag of a few millis
+    // currently using concat cause setCourseGoals needs a new array to update
+    // setCourseGoals(courseGoals.concat(newGoal));
+
+    // this handles it better as the state update will depend on if the previous state data changes
+    // setCourseGoals((prevCourseGoals) => {
+    //   return prevCourseGoals.concat(newGoal);
+    // });
+    // can be shortened to since there's only one expression
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
+  };
+
+  // Pass functions down to component to handle the adding of the name then the handler pushes to courseGoals
+  return (
+  <div className="course-goals">
     <h2> Course Goals</h2>
-    <NewGoal />
+    <NewGoal onAddGoal={addNewGoalHandler} />
     <GoalList goals={courseGoals}/>
   </div>
+  );
 }
 
 export default App;
--- react-refresher-01-react-starting-project/src/components/NewGoal/NewGoal.js
+++ react-refresher-01-react-starting-project/src/components/NewGoal/NewGoal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './NewGoal.css';
 
 
@@ -13,21 +13,35 @@ import './NewGoal.css';
 //     );
 // };
 
-const NewGoal = () => {
+const NewGoal = props => {
+
+    const [enteredText, setEnteredText] = useState('');
+
     const addGoalHandler = event => {
         event.preventDefault();
 
         const newGoal = {
             id: Math.random().toString(),
-            text: 'My new goal!'
+            text: enteredText
         };
 
-        console.log(newGoal);
+        setEnteredText('');
+
+        // Holds a pointer to the AddNewGoalHandler. so we can execute on AddGoal
+        props.onAddGoal(newGoal);
+    };
+
+    const textChangeHandler = event => {
+        setEnteredText(event.target.value);
     };
 
     return (
+        // For value,
+        // we're binding the value of the input on every keystroke 
+        // and we're updating the value which we then bind back to the input
+        // This is called two-way binding
         <form className="new-goal" onSubmit={addGoalHandler}>
-            <input type="text"/>
+            <input type="text" value={enteredText} onChange={textChangeHandler} />
             <button type="submit">Add Goal</button>
         </form>
     );
