7852e972fb209e67b39abdb2d334a1290141f345
Rayho564 <rayho564@gmail.com>
2022-09-29 00:17:56 -0700

    Bug fixes

--- NEBackend/controllers/users-controllers.js
+++ NEBackend/controllers/users-controllers.js
@@ -18,6 +18,11 @@ const getUsers = (req, res, next) => {
 const signup = (req, res, next) => {
   const { name, email, password } = req.body;
 
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("Could not create user, email already exists.", 422);
+  }
+
   const createdUser = {
     id: uuid(),
     name,
@@ -34,13 +39,14 @@ const login = (req, res, next) => {
   const { email, password } = req.body;
 
   //Will be refined later for auth
-  const identifiedUser = DUMMY_USERS.filter((u) => {
-    u.email === email;
-  });
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
 
   if (!identifiedUser || identifiedUser.password !== password) {
     // 401 means auth fail
-    throw new HttpError("Could not identify user, credentials seem to be wrong", 401);
+    throw new HttpError(
+      "Could not identify user, credentials seem to be wrong",
+      401
+    );
   }
 
   res.json({ message: "Logged in!" });
