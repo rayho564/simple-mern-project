019849be7febe271f63af926fc542106de000cf7
Rayho564 <rayho564@gmail.com>
2022-09-29 00:13:05 -0700

    Adding in user controllers

---
+++ NEBackend/controllers/users-controllers.js
@@ -0,0 +1,51 @@
+const { v4: uuid } = require("uuid"); // V4 includes a timestamp
+
+const HttpError = require("../models/http-error");
+
+let DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "This Name",
+    email: "text@test.com",
+    password: "testers",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const createdUser = {
+    id: uuid(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Will be refined later for auth
+  const identifiedUser = DUMMY_USERS.filter((u) => {
+    u.email === email;
+  });
+
+  if (!identifiedUser || identifiedUser.password !== password) {
+    // 401 means auth fail
+    throw new HttpError("Could not identify user, credentials seem to be wrong", 401);
+  }
+
+  res.json({ message: "Logged in!" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
