dd776b573cec48ed377847fdbce6a0520df42e53
Rayho564 <rayho564@gmail.com>
2022-10-17 20:52:28 -0700

    Starting to add in Authentication with bcryptjs

--- NEBackend/controllers/users-controllers.js
+++ NEBackend/controllers/users-controllers.js
@@ -1,6 +1,8 @@
 //const { v4: uuid } = require("uuid"); // V4 includes a timestamp
 const { validationResult } = require("express-validator");
 
+const bcrypt = require("bcryptjs");
+
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
 
@@ -65,11 +67,22 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  // Salt -> salting rounds = reiterate the hashing
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not create user, please try again.",
+      500
+    );
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
-    // Later we'll add in encryption for the password later
-    password,
+    password: hashedPassword,
     image: req.file.path,
     places: [],
   });
@@ -106,9 +119,34 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  // A dummy validation for now to check if email or password is wrong
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not create user, please try again.",
+      500
+    );
+    return next(error);
+  }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
+    const error = new HttpError("Invalid credentials could not log in.", 401);
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
     const error = new HttpError("Invalid credentials could not log in.", 401);
     return next(error);
   }
